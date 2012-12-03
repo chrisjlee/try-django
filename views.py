bf8beb0e205c2d18a41a52beeaf0cab1578fe0c9
Chris Lee <chrisl@getlevelten.com>
2012-12-03 15:26:58 -0600

    Rewrite to be more succinct and utilize render_to_response function

--- views.py
+++ views.py
@@ -1,13 +1,9 @@
-from django.template.loader import get_template
-from django.template import Template, Context
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    return render_to_response('current_datetime.html', {'current_date': now})
 def hello(request):
     return HttpResponse("Hello world")
 def hours_ahead(request, offset):
