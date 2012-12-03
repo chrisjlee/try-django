53114d23393f48c455219061543c92aa55199707
Chris Lee <chrisl@getlevelten.com>
2012-12-03 14:55:19 -0600

    Added time head dynamic url

--- urls.py
+++ urls.py
@@ -1,24 +1,8 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
-)
-
 from django.conf.urls.defaults import *
-from myproject.views import hello
+from myproject.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
-    ('^hello/$', hello),
+    (r'^hello/$', hello),
+    (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
