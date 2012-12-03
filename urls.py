0f32d1ab2aa2f6aa2499d3adc8cfe098e6f53070
Chris Lee <chrisl@getlevelten.com>
2012-12-03 15:24:16 -0600

    add template

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
-from myproject.views import hello, current_datetime, hours_ahead
+from myproject.views import hello, current_datetime, hours_ahead, chapter_request
 
 urlpatterns = patterns('',
     (r'^hello/$', hello),
     (r'^time/$', current_datetime),
     (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    (r'^status/$', chapter_request),
 )
--- views.py
+++ views.py
@@ -1,8 +1,12 @@
+from django.template.loader import get_template
+from django.template import Template, Context
 from django.http import HttpResponse
 import datetime
+
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
     return HttpResponse(html)
 def hello(request):
     return HttpResponse("Hello world")
@@ -12,5 +16,19 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    assert False
     html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+#def hours_ahead(request, offset):
+#    try:
+#        offset = int(offset)
+#    except ValueError:
+#        raise Http404()
+#    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+#    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+#    return HttpResponse(html)
+def chapter_request(request):
+    current_chapter = 'Chapter 4'
+    url = 'http://www.djangobook.com/en/2.0/chapter04.html'
+    html = "<html><body>Current Chapter is: <a href='%s'><b>%s</b></a>.</body></html>" % (url, current_chapter)
     return HttpResponse(html)
